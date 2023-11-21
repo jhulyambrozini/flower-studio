51cf49600d4a7e7dc1d4937e01e358e615bc4d7f
jhuly ambrozini <jhulyamfebr@gmial.com>
2023-11-20 22:32:02 -0300

    fix: otimizando imagens

---
+++ src/scripts.js
@@ -0,0 +1,47 @@
+
+function validate(elem) {
+   if (elem.val() == '') {
+
+      console.log('o campo de ' + elem.attr('name') + ' é obrigatório')
+
+      elem.parent().find('.text-muted').show()
+
+      elem.addClass('invalid')
+
+      return false
+   } else {
+      elem.parent().find('.text-muted').hide()
+      elem.removeClass('invalid')
+   }
+}
+
+$('form').on('submit', function (e) {
+
+   e.preventDefault()
+
+   const inputName = $('#name')
+   const inputEmail = $('#email')
+
+   validate(inputName)
+   validate(inputEmail)
+
+   if (inputEmail.hasClass('invalid') || inputName.hasClass('invalid')) {
+      console.log('verificar campos obrigatórios')
+      return false
+   } else {
+      $(this).submit()
+   }
+
+})
+
+$('body').on('blur', '#name', function () {
+   validate($(this))
+})
+
+$('body').on('blur', '#email', function () {
+   validate($(this))
+})
+
+$('body').on('blur', '#msg', function () {
+   validate($(this))
+})
